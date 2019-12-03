75aa14a5e05d18c3dbdc362e3da0e684bc6ac238
olshkol <olegskolnikov@gmail.com>
2019-12-03 16:49:34 +0200

    add Auth Service

---
+++ ms-auth-server/src/main/java/olshkol/msauthserver/MsAuthServerApplication.java
@@ -0,0 +1,13 @@
+package olshkol.msauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAuthServerApplication.class, args);
+    }
+
+}
---
+++ ms-auth-server/src/main/java/olshkol/msauthserver/entity/Role.java
@@ -0,0 +1,6 @@
+package olshkol.msauthserver.entity;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
